c0c435274b93baf8055c070aea16a549ca50ab87
krislm <mcabe@me.com>
2020-07-08 03:33:44 +0200

    Updates

---
+++ precache-manifest.abcbeba4817291bc09498deae1c3dbbe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80e1c14ce2d426f57d35a8956dcf219",
+    "url": "/dwarf-code-test/index.html"
+  },
+  {
+    "revision": "338e0f75b6f30b94713e",
+    "url": "/dwarf-code-test/static/css/2.a7bca8fd.chunk.css"
+  },
+  {
+    "revision": "8ef16b3fe2c3da7ae6ae",
+    "url": "/dwarf-code-test/static/css/main.2b0d8a3a.chunk.css"
+  },
+  {
+    "revision": "338e0f75b6f30b94713e",
+    "url": "/dwarf-code-test/static/js/2.497123c6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dwarf-code-test/static/js/2.497123c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef16b3fe2c3da7ae6ae",
+    "url": "/dwarf-code-test/static/js/main.fae42c06.chunk.js"
+  },
+  {
+    "revision": "a91e4ccff27d77e9c8e9",
+    "url": "/dwarf-code-test/static/js/runtime-main.1088617d.js"
+  },
+  {
+    "revision": "86f3ed7ba3f60ba1d1ca381bb9372893",
+    "url": "/dwarf-code-test/static/media/burger.86f3ed7b.svg"
+  },
+  {
+    "revision": "ec00cfafcb2953463eb1f9142ac40398",
+    "url": "/dwarf-code-test/static/media/hotdog.ec00cfaf.svg"
+  },
+  {
+    "revision": "dc945fa3e6a929879298e3ae0a53d8c7",
+    "url": "/dwarf-code-test/static/media/info.dc945fa3.svg"
+  },
+  {
+    "revision": "c8f375f71ebb8637f6c8441f2ec309cd",
+    "url": "/dwarf-code-test/static/media/logo.c8f375f7.svg"
+  },
+  {
+    "revision": "086db7ac93c4807b9a7468cff4d742ae",
+    "url": "/dwarf-code-test/static/media/pizza.086db7ac.svg"
+  }
+]);
